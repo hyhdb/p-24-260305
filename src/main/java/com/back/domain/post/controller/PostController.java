05c2cd590cec86ccc91fbea51c982e34dc885008
hyhdb <kkminwoo@gmail.com>
2026-03-05 12:50:57 +0900

    7

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -18,8 +18,37 @@ public class PostController {
     @ResponseBody
     public String writeForm() {
 
+        return getWriteForm();
+    }
+
+    @PostMapping("/posts/write")
+    @ResponseBody
+    public String write(String title, String content) {
+
+        // 유효성 체크
+        if(title.isBlank()) {
+            return """
+                    <div style="color:red">제목을 입력해주세요.</div>
+                    %s
+                    """.formatted(getWriteForm());
+        }
+
+        if(content.isBlank()) {
+            return """
+                    <div style="color:red">내용을 입력해주세요.</div>
+                    %s
+                    """.formatted(getWriteForm());
+        }
+        //
+
+        Post post = postService.write(title, content);
+
+        return "%d번 글이 작성되었습니다.".formatted(post.getId());
+    }
+
+    private String getWriteForm() {
         return """
-                <form method = "post" action="/posts/write">
+                <form method="post" action="/posts/write">
                   <input type="text" name="title">
                   <br>
                   <textarea name="content"></textarea>
@@ -27,15 +56,6 @@ public class PostController {
                   <input type="submit" value="작성">
                 </form>
                 """;
-    }
-
-    @PostMapping("/posts/write")
-    @ResponseBody
-    public String write(String title, String content) {
-
-        Post post = postService.write(title, content);
-
-        return "%d번 글이 작성되었습니다.".formatted(post.getId());
     }
 
 }
