41f9f3bf936cbfac8420b94eefa8028fe34bd2f1
hyhdb <kkminwoo@gmail.com>
2026-03-06 11:31:25 +0900

    17

--- src/main/java/com/back/domain/post/controller/PostController.java
+++ src/main/java/com/back/domain/post/controller/PostController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.stream.Collectors;
 
@@ -22,10 +21,8 @@ public class PostController {
     private final PostService postService;
 
     @GetMapping("/posts/write-form")
-    @ResponseBody
     public String writeForm() {
-
-        return getWriteForm("", "", "");
+        return "write";
     }
 
     @AllArgsConstructor
@@ -40,7 +37,6 @@ public class PostController {
     }
 
     @PostMapping("/posts/write")
-    @ResponseBody
     public String write(@Valid WriteRequestForm form, BindingResult bindingResult) {
 
         if(bindingResult.hasErrors()) {
@@ -55,35 +51,14 @@ public class PostController {
                     .sorted()
                     .collect(Collectors.joining("\n"));
 
-            return getWriteForm(errorMessages, form.title, form.content);
+            // 템플릿 응답
+            return "write";
         }
 
         Post post = postService.write(form.title, form.content);
 
+        // 템플릿 응답
         return "%d번 글이 작성되었습니다.".formatted(post.getId());
-    }
-
-    private String getWriteForm(String errorMessage, String title, String content) {
-        return """
-                <ul style="color:red">%s</ul>
-                <form method="post" action="/posts/write">
-                  <input type="text" name="title" value="%s" autoFocus>
-                  <br>
-                  <textarea name="content">%s</textarea>
-                  <br>
-                  <input type="submit" value="작성">
-                </form>
-                
-                <script>
-                    const li = document.querySelector("ul li");
-                    const errorFieldName = li.dataset.errorField;
-                
-                    if(errorFieldName.length > 0) {
-                        const form = document.querySelector("form");
-                        form[errorFieldName].focus();
-                    }
-                </script>
-                """.formatted(errorMessage, title, content);
     }
 
 }
